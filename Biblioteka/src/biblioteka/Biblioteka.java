678f5b62e3955ba4e515de1ae6b2a4fc813238c6
ivonaH <ivonaheldrih@gmail.com>
2020-04-11 19:25:18 +0200

    Implementirane metode iz interfejsa BibliotekaInterfejs u klasi Biblioteka i dodata gson biblioteka.

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -1,35 +1,43 @@
 package biblioteka;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import biblioteka.interfejs.BibliotekaInterfejs;
 
 /**
- * Klasa predstavlja biblioteku sa knjigama. Knjige se cuvaju u
- * listi. Klasa implementira interfejs BibliotekaInterfejs.
+ * Klasa predstavlja biblioteku sa knjigama. Knjige se cuvaju u listi. Klasa
+ * implementira interfejs BibliotekaInterfejs.
  * 
  * @author Bojan Tomic
  */
 public class Biblioteka implements BibliotekaInterfejs {
-	
+
 	/**
 	 * Lista sa knjigama iz biblioteke.
 	 */
 	private List<Knjiga> knjige = new ArrayList<Knjiga>();
 
 	/**
-	 * Dodaje novu knjigu u biblioteku. Knjiga se dodaje na kraj
-	 * liste knjiga.
+	 * Dodaje novu knjigu u biblioteku. Knjiga se dodaje na kraj liste knjiga.
 	 * 
 	 * @param knjiga Nova knjiga koja se dodaje u biblioteku.
 	 * 
 	 * @throws java.lang.RuntimeException Ako je:
-	 * <ul>
-	 * 	<li> uneta knjiga null ili
-	 *  <li> ako vec postoji ista knjiga u biblioteci
-	 * </ul>
+	 *                                    <ul>
+	 *                                    <li>uneta knjiga null ili
+	 *                                    <li>ako vec postoji ista knjiga u
+	 *                                    biblioteci
+	 *                                    </ul>
 	 * 
 	 * @see <a href = "http://ai.fon.bg.ac.rs/">AI SAJT</a>
 	 */
@@ -37,7 +45,7 @@ public class Biblioteka implements BibliotekaInterfejs {
 	public void dodajKnjigu(Knjiga knjiga) {
 		if (knjiga == null)
 			throw new RuntimeException("Knjiga ne sme biti null");
-		
+
 		if (knjige.contains(knjiga))
 			throw new RuntimeException("Knjiga vec postoji u biblioteci");
 
@@ -55,7 +63,7 @@ public class Biblioteka implements BibliotekaInterfejs {
 	public void obrisiKnjigu(Knjiga knjiga) {
 		if (knjiga == null)
 			throw new RuntimeException("Knjiga ne sme biti null");
-		
+
 		knjige.remove(knjiga);
 	}
 
@@ -70,32 +78,53 @@ public class Biblioteka implements BibliotekaInterfejs {
 	}
 
 	/**
-	 * Pretrazuje biblioteku po zadatim parametrima i vraca
-	 * listu knjiga koje odgovaraju pretrazi. Ako se neki
-	 * parametar izostavi (prosledi se null), pretrazivanje
-	 * se vrsi po ostalim parametrima.
+	 * Pretrazuje biblioteku po zadatim parametrima i vraca listu knjiga koje
+	 * odgovaraju pretrazi. Ako se neki parametar izostavi (prosledi se null),
+	 * pretrazivanje se vrsi po ostalim parametrima.
 	 * 
-	 * @param autor Autor knjige kao kriterijum
-	 * @param isbn	ISBN knjige kao kriterijum
-	 * @param naslov Naslov knjige kao kriterijum (moze
-	 *  i samo deo naslova)
+	 * @param autor   Autor knjige kao kriterijum
+	 * @param isbn    ISBN knjige kao kriterijum
+	 * @param naslov  Naslov knjige kao kriterijum (moze i samo deo naslova)
 	 * @param izdavac Izdavac kao kriterijum
 	 * 
-	 * @return Lista knjiga koje odgovaraju kriterijumima ili null
-	 * ako su svi kriterijumi pretrage uneto kao null.
+	 * @return Lista knjiga koje odgovaraju kriterijumima ili null ako su svi
+	 *         kriterijumi pretrage uneto kao null.
 	 */
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
-		if (autor == null && naslov == null && isbn == null	&& izdavac == null)
-			return null;		
-		
+		if (autor == null && naslov == null && isbn == null && izdavac == null)
+			return null;
+
 		List<Knjiga> rezultat = new LinkedList<Knjiga>();
-		
-		for(Knjiga k: knjige)
+
+		for (Knjiga k : knjige)
 			if (k.getNaslov().contains(naslov))
 				rezultat.add(k);
-		
+
 		return rezultat;
+	}
+
+	@Override
+	public void sacuvajSve(String nazivFajla) {
+		Gson gson = new Gson();
+		try (FileWriter out = new FileWriter(nazivFajla)) {
+			gson.toJson(knjige, out);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void ucitajSveKnjige(String nazivFajla) {
+		Gson gson = new Gson();
+		try (FileReader in = new FileReader(nazivFajla)) {
+			knjige = null;
+			Type collectionType = new TypeToken<ArrayList<Knjiga>>() {}.getType();
+			knjige = gson.fromJson(nazivFajla, collectionType);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
